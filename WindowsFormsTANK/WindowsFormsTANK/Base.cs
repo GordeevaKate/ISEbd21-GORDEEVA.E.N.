e1b41eeb94620d36a8f748791fed56181aec31a4
maa.vaa.81@mail.ru <maa.vaa.81@mail.ru>
2019-11-22 19:06:46 +0400

    1

---
+++ WindowsFormsTANK/WindowsFormsTANK/Base.cs
@@ -0,0 +1,86 @@
+﻿using System.Drawing;
+
+
+    public class Base<T> where T : class, ITransport
+    {
+    private T[] _places;
+    private int PictureWidth { get; set; }
+   private int PictureHeight { get; set; }
+   private const int _placeSizeWidth = 210;
+ 
+    private const int _placeSizeHeight  =80;
+    public Base(int sizes, int pictureWidth, int pictureHeight)
+    {
+        _places = new T[sizes];
+        PictureWidth = pictureWidth;
+        PictureHeight = pictureHeight;
+        for (int i = 0; i < _places.Length; i++)
+        {
+            _places[i] = null;
+        }
+    }
+    public static int operator +(Base<T> p, T tank)
+    {
+        for (int i = 0; i < p._places.Length; i++)
+        {
+            if (p.CheckFreePlace(i))
+            {
+                p._places[i] = tank;
+                p._places[i].SetPosition(-90 + i / 5 * _placeSizeWidth + 5,
+                 i % 5 * _placeSizeHeight + 28, p.PictureWidth,
+                p.PictureHeight);
+                return i;
+            }
+        }
+        return -1;
+    }
+ public static T operator -(Base<T> p, int index)
+    {
+        if (index < 0 || index > p._places.Length)
+        {
+            return null;
+        }
+        if (!p.CheckFreePlace(index))
+         
+ {
+            T tank = p._places[index-1];
+            p._places[index-1] = null;
+            return tank;
+        }
+        return null;
+    }
+ private bool CheckFreePlace(int index)
+    {
+        return _places[index] == null;
+    }
+ 
+    public void Draw(Graphics g)
+    {
+        DrawMarking(g);
+        for (int i = 0; i < _places.Length; i++)
+        {
+            if (!CheckFreePlace(i))
+            {
+                _places[i].DrawTank(g);
+            }
+        }
+    }
+   
+    private void DrawMarking(Graphics g)
+    {
+        Pen pen = new Pen(Color.Black, 3);
+  
+        g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
+        for (int i = 0; i < _places.Length / 5; i++)
+        {
+            for (int j = 0; j < 6; ++j)
+            {
+                g.DrawLine(pen, i * _placeSizeWidth, j * _placeSizeHeight,
+                i * _placeSizeWidth + 110, j * _placeSizeHeight);
+            }
+            g.DrawLine(pen, i * _placeSizeWidth, 0, i * _placeSizeWidth, 400);
+        }
+    }
+}
+    
+
---
+++ WindowsFormsTANK/WindowsFormsTANK/FormBase.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsTANK
+{
+    partial class FormBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxBase = new System.Windows.Forms.PictureBox();
+            this.buttonSetTank = new System.Windows.Forms.Button();
+            this.buttonSetVehicle = new System.Windows.Forms.Button();
+            this.groupBoxBase = new System.Windows.Forms.GroupBox();
+            this.labelBase = new System.Windows.Forms.Label();
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.labelBaseTank = new System.Windows.Forms.Label();
+            this.buttonTake = new System.Windows.Forms.Button();
+            this.pictureBoxTakeTank = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBase)).BeginInit();
+            this.groupBoxBase.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeTank)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxBase
+            // 
+            this.pictureBoxBase.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxBase.Name = "pictureBoxBase";
+            this.pictureBoxBase.Size = new System.Drawing.Size(873, 506);
+            this.pictureBoxBase.TabIndex = 0;
+            this.pictureBoxBase.TabStop = false;
+            // 
+            // buttonSetTank
+            // 
+            this.buttonSetTank.Location = new System.Drawing.Point(904, 33);
+            this.buttonSetTank.Name = "buttonSetTank";
+            this.buttonSetTank.Size = new System.Drawing.Size(102, 37);
+            this.buttonSetTank.TabIndex = 1;
+            this.buttonSetTank.Text = "припарковать танк";
+            this.buttonSetTank.UseVisualStyleBackColor = true;
+            this.buttonSetTank.Click += new System.EventHandler(this.ButtonSetTank_Click);
+            // 
+            // buttonSetVehicle
+            // 
+            this.buttonSetVehicle.Location = new System.Drawing.Point(904, 96);
+            this.buttonSetVehicle.Name = "buttonSetVehicle";
+            this.buttonSetVehicle.Size = new System.Drawing.Size(101, 41);
+            this.buttonSetVehicle.TabIndex = 2;
+            this.buttonSetVehicle.Text = "припарковать бронемашину";
+            this.buttonSetVehicle.UseVisualStyleBackColor = true;
+            this.buttonSetVehicle.Click += new System.EventHandler(this.ButtonSetVehicle_Click);
+            // 
+            // groupBoxBase
+            // 
+            this.groupBoxBase.Controls.Add(this.pictureBoxTakeTank);
+            this.groupBoxBase.Controls.Add(this.buttonTake);
+            this.groupBoxBase.Controls.Add(this.labelBaseTank);
+            this.groupBoxBase.Controls.Add(this.maskedTextBox);
+            this.groupBoxBase.Controls.Add(this.labelBase);
+            this.groupBoxBase.Location = new System.Drawing.Point(879, 211);
+            this.groupBoxBase.Name = "groupBoxBase";
+            this.groupBoxBase.Size = new System.Drawing.Size(180, 295);
+            this.groupBoxBase.TabIndex = 3;
+            this.groupBoxBase.TabStop = false;
+            // 
+            // labelBase
+            // 
+            this.labelBase.AutoSize = true;
+            this.labelBase.Location = new System.Drawing.Point(15, 21);
+            this.labelBase.Name = "labelBase";
+            this.labelBase.Size = new System.Drawing.Size(75, 13);
+            this.labelBase.TabIndex = 0;
+            this.labelBase.Text = "Забрать танк";
+            // 
+            // maskedTextBox
+            // 
+            this.maskedTextBox.Location = new System.Drawing.Point(93, 49);
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.Size = new System.Drawing.Size(33, 20);
+            this.maskedTextBox.TabIndex = 1;
+            // 
+            // labelBaseTank
+            // 
+            this.labelBaseTank.AutoSize = true;
+            this.labelBaseTank.Location = new System.Drawing.Point(12, 53);
+            this.labelBaseTank.Name = "labelBaseTank";
+            this.labelBaseTank.Size = new System.Drawing.Size(38, 13);
+            this.labelBaseTank.TabIndex = 2;
+            this.labelBaseTank.Text = "место";
+            // 
+            // buttonTake
+            // 
+            this.buttonTake.Location = new System.Drawing.Point(23, 89);
+            this.buttonTake.Name = "buttonTake";
+            this.buttonTake.Size = new System.Drawing.Size(91, 22);
+            this.buttonTake.TabIndex = 3;
+            this.buttonTake.Text = "Забрать";
+            this.buttonTake.UseVisualStyleBackColor = true;
+            this.buttonTake.Click += new System.EventHandler(this.ButtonTake_Click);
+            // 
+            // pictureBoxTakeTank
+            // 
+            this.pictureBoxTakeTank.Location = new System.Drawing.Point(5, 132);
+            this.pictureBoxTakeTank.Name = "pictureBoxTakeTank";
+            this.pictureBoxTakeTank.Size = new System.Drawing.Size(175, 163);
+            this.pictureBoxTakeTank.TabIndex = 4;
+            this.pictureBoxTakeTank.TabStop = false;
+            // 
+            // FormBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 506);
+            this.Controls.Add(this.groupBoxBase);
+            this.Controls.Add(this.buttonSetVehicle);
+            this.Controls.Add(this.buttonSetTank);
+            this.Controls.Add(this.pictureBoxBase);
+            this.Name = "FormBase";
+            this.Text = "База";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBase)).EndInit();
+            this.groupBoxBase.ResumeLayout(false);
+            this.groupBoxBase.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeTank)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxBase;
+        private System.Windows.Forms.Button buttonSetTank;
+        private System.Windows.Forms.Button buttonSetVehicle;
+        private System.Windows.Forms.GroupBox groupBoxBase;
+        private System.Windows.Forms.Label labelBase;
+        private System.Windows.Forms.PictureBox pictureBoxTakeTank;
+        private System.Windows.Forms.Button buttonTake;
+        private System.Windows.Forms.Label labelBaseTank;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox;
+    }
+}
