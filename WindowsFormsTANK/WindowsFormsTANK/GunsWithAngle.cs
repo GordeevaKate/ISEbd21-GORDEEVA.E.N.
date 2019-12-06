73dba2deb8aec5bae041110d6752fcd89c6ffa97
maa.vaa.81@mail.ru <maa.vaa.81@mail.ru>
2019-12-06 20:15:56 +0400

    1

--- WindowsFormsTANK/WindowsFormsTANK/GunsWithAngle.cs
+++ WindowsFormsTANK/WindowsFormsTANK/GunsWithAngle.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 public class GunsWithAngle : IGuns
 {
@@ -18,29 +18,21 @@ public class GunsWithAngle : IGuns
     {
         int n = (int)numberOfGuns;
         Brush br1 = new SolidBrush(color);
-        g.FillRectangle(br1, _startPosX + 22, _startPosY + 5, 47, 15);
-        Brush br2 = new SolidBrush(Color.Orange);
-        g.FillRectangle(br2, _startPosX + 25, _startPosY + 8, 25, 1);
-        g.FillRectangle(br2, _startPosX + 25, _startPosY + 10, 25, 1);
-        g.FillRectangle(br2, _startPosX + 25, _startPosY + 12, 25, 1);
-        Brush br3 = new SolidBrush(Color.Black);
-        g.FillRectangle(br3, _startPosX + 25, _startPosY + 9, 25, 1);
-        g.FillRectangle(br3, _startPosX + 25, _startPosY + 11, 25, 1);
-        Pen pen = new Pen(Color.Black);
-        if (n == 0)
+         Brush br2 = new SolidBrush(Color.Orange);
+         if (n >= 0)
         {
-            g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
-        }
-        if (n == 1)
-        {
-            g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
-            g.DrawLine(pen, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 6);
-        }
-        if (n == 2)
-        {
-            g.DrawLine(pen, _startPosX + 69, _startPosY + 12, _startPosX + 89, _startPosY + 9);
-            g.DrawLine(pen, _startPosX + 69, _startPosY + 9, _startPosX + 89, _startPosY + 6);
-            g.DrawLine(pen, _startPosX + 69, _startPosY + 15, _startPosX + 89, _startPosY + 12);
+            g.FillRectangle(br1, _startPosX + 145, _startPosY - 20, 35, 4);
+            g.FillRectangle(br2, _startPosX + 170, _startPosY - 22, 20, 8);
+            if (n >= 1)
+            {
+                g.FillRectangle(br1, _startPosX + 160, _startPosY - 10, 35, 8);
+                g.FillRectangle(br2, _startPosX + 195, _startPosY - 12, 20, 12);
+                if (n >= 2)
+                {
+                    g.FillRectangle(br1, _startPosX + 185, _startPosY + 10, 35, 8);
+                    g.FillRectangle(br2, _startPosX + 220, _startPosY + 8, 20, 12);
+                }
+            }
         }
     }
 }
